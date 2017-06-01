913f2ab89896bc47692b5a1a3d0671ef3978a389
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-06-01 15:37:11 +0200

    leap

---
+++ elixir/leap/leap.exs
@@ -0,0 +1,20 @@
+defmodule Year do
+  @doc """
+  Returns whether 'year' is a leap year.
+
+  A leap year occurs:
+
+  on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+      unless the year is also evenly divisible by 400
+  """
+  @spec leap_year?(non_neg_integer) :: boolean
+  def leap_year?(year) do
+    leap(rem(year, 4), rem(year, 100), rem(year, 400))
+  end
+
+  defp leap(0, 0, 0), do: true
+  defp leap(0, 0, _), do: false
+  defp leap(0, _, _), do: true
+  defp leap(_, _, _), do: false
+end
